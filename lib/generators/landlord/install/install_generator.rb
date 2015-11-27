407e75304d24c3157409736e3748eacd82443598
Andrew J. Ayers <andy@ajayers.net>
2015-11-27 16:26:51 -0600

    Added seeds template to install generator

--- lib/generators/landlord/install/install_generator.rb
+++ lib/generators/landlord/install/install_generator.rb
@@ -2,12 +2,16 @@ module Landlord
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
 
-    def copy_files
+    def copy_initializer
       template "landlord.rb", File.join("config", "initializers", "landlord.rb")
     end
 
     def create_directory
       empty_directory File.join("db", "landlord", "migrate")
+    end
+
+    def copy_seeds
+      template "seeds.rb", File.join("db", "landlord", "seeds.rb")
     end
   end
 end
---
+++ lib/generators/landlord/install/templates/seeds.rb
@@ -0,0 +1,7 @@
+# This file should contain all the creation needed to seed a tenant schema with its default values.
+# The data can then be loaded with the rake landlord:seed
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
